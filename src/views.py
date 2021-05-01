c91083429964a261047f477e5017ccc65031ed88
gourish13 <sadhu5gourish@gmail.com>
2021-05-01 15:44:20 +0530

    desc added

--- src/views.py
+++ src/views.py
@@ -1,9 +1,10 @@
 """
-Views 
+View related route controllers
 """
 
 from flask import render_template
 
-
+# @desc     Home Page
+# @route    GET /
 def index():
     return render_template('index.html')
