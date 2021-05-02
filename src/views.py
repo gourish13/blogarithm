8c6e948c9a0eef3a86411e20de0003930f7e30ba
Prasad Biswas <alterego14@pop-os.localdomain>
2021-05-02 19:05:42 +0100

    blogview

--- src/views.py
+++ src/views.py
@@ -8,3 +8,6 @@ from flask import render_template
 # @route    GET /
 def index():
     return render_template('index.html')
+
+def blog_view(slug):
+    return render_template('blogview.html' , title=title , content=blogcontent)
