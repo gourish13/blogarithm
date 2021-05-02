328994d6669e2a6c15225e2c96f5c7d8b6e2acdb
gourish13 <sadhu5gourish@gmail.com>
2021-05-03 00:36:06 +0530

    styles not working❌ ❌

--- src/views.py
+++ src/views.py
@@ -2,7 +2,14 @@
 View related route controllers
 """
 
-from flask import render_template
+from flask import (
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+)
+
 
 # @desc     Home Page
 # @route    GET /
@@ -10,16 +17,25 @@ def index():
     return render_template('index.html')
 
 
-#Create View function
+
+# @desc     Blog Create Page
+# @route    GET /blog/<string:slug>
 def blog_create():
     return render_template('blog-edit.html') 
 
-#Update View function     
-def blog_update(title): 
+
+
+# @desc     Blog Update Page
+# @route    GET /blog-create
+def blog_update(slug): 
     return render_template('blog-edit.html') 
+
+
+
 # @desc     Blog View Page
-# @route    GET /blog/<string:slug>
+# @route    GET /blog-update/<string:slug>
 def blog_view(slug):
     return render_template('blogview.html' , title='title' , content='blogcontent')
+
 
 
