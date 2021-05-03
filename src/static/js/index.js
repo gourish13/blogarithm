2fbbc858935a78a841197bb8fdf056025e9e6067
gourish13 <sadhu5gourish@gmail.com>
2021-05-03 21:05:35 +0530

    working on home page

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,2 +1,11 @@
-
+(function (){
+  window.onload = function() {
+    let nav = document.getElementsByTagName('nav')[0];
+    window.onscroll = function() {
+      let scrollVal = window.pageYOffset;
+      if (scrollVal === 0)
+        nav.classList = ['nav-hidden'];
+    }
+  }
+})();
 
