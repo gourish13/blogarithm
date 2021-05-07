9f22754b3ed7d127cb5f943476cc07462a41406c
gourish13 <sadhu5gourish@outlook.com>
2021-05-08 03:12:13 +0530

    created user model

--- src/__init__.py
+++ src/__init__.py
@@ -17,19 +17,17 @@ def app_factory():
             blog_create,
             blog_update,
             privacy_policy,
-
     )
-    
-    from .auth import (
-        auth
-     )
     app.add_url_rule('/', view_func=index)
-    app.add_url_rule('/auth' , view_func=auth)
     app.add_url_rule('/blog-create', view_func=blog_create, methods=['GET', 'POST'])
     app.add_url_rule('/blog-update/<string:slug>', view_func=blog_update, methods=['GET', 'POST'])
     app.add_url_rule('/blog/<string:slug>', view_func=blog_view)
     app.add_url_rule('/privacy-policy', view_func=privacy_policy)
 
 # Auth Routes
+    from .auth import (
+        auth
+     )
+    app.add_url_rule('/auth' , view_func=auth)
 
     return app
--- src/models.py
+++ src/models.py
@@ -1,2 +1,21 @@
+"""
+Database models
+"""
+
+from os import environ
+from pydal import DAL, Field
+
+DB_URI = environ['DB_URI']
+
+db = DAL(DB_URI, pool_size=5)
+
+db.define_table('users',
+	Field('name', type='string', required=True, notnull=True),
+	Field('email', type='string', required=True, notnull=True, unique=True),
+	Field('password', type='string', required=False, notnull=False, default=None),
+	Field('role', type='string', required=True, notnull=True, default='user')
+	)
 
 
+
+db.commit()
