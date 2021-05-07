aae451387aab11afcb1b736e6289a117a37a152e
Rupam <www.rupamdasgupta@gmail.com>
2021-05-07 20:52:24 +0530

    auth route

--- src/__init__.py
+++ src/__init__.py
@@ -13,11 +13,14 @@ def app_factory():
 # View Routes
     from .views import (
             index,
+            auth,
             blog_view , 
             blog_create , 
             blog_update
+          
     )
     app.add_url_rule('/', view_func=index)
+    app.add_url_rule('/auth' , view_func=auth)
     app.add_url_rule('/blog-create', view_func=blog_create, methods=['GET', 'POST'])
     app.add_url_rule('/blog-update/<string:slug>', view_func=blog_update, methods=['GET', 'POST'])
     app.add_url_rule('/blog/<string:slug>', view_func=blog_view)
