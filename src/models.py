e626e4e9fdcf6f5caa4c364060182018fd69d3cc
Rupam <www.rupamdasgupta@gmail.com>
2021-05-08 19:30:20 +0530

    blogs table created

--- src/models.py
+++ src/models.py
@@ -5,9 +5,9 @@ Database models
 from os import environ
 from pydal import DAL, Field
 
-DB_URI = environ['DB_URI']
+DB_URI = environ['DATABASE_URL']
 
-db = DAL(DATABASE_URL, pool_size=5)
+db = DAL(DB_URI, pool_size=5)
 
 db.define_table('users',
 	Field('name', type='string', required=True, notnull=True),
@@ -16,6 +16,16 @@ db.define_table('users',
 	Field('role', type='string', required=True, notnull=True, default='user')
 	)
 
-
+db.define_table('blogs' , 
+    Field('title' , type='string' , required=True , notnull=True) , 
+    Field('content',type='text' , required=True , notnull=True) ,
+    Field('posted_on',type='date' , required=True , notnull=True) ,
+    Field('author',type='string' , required=True ,    notnull=True) ,
+    Field('users',type='reference users' , required=True ,    notnull=True , ondelete='CASCADE') ,
+    Field('reported',type='boolean' , required=True , notnull=True , default=False) ,
+    Field('slug',type='string' , required=True , notnull=True) ,
+    Field('private',type='boolean' , required=True , notnull=True , default=False)
+    ) 
+   
 
 db.commit()
