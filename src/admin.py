fede484330710ee2f7d478cbfa83799a7d3f03fb
gourish13 <sadhu5gourish@outlook.com>
2021-05-09 19:02:16 +0530

    can now create superuser

---
+++ src/admin.py
@@ -0,0 +1,20 @@
+"""
+Admin Controllers
+"""
+
+from werkzeug.security import generate_password_hash
+from .models import new_user
+
+def new_superuser():
+	from getpass import getpass
+	from sys import exit
+	name = input('Name: ')
+	email = input('Email: ')
+	while True:
+		password 				= getpass()
+		if password == getpass('Confirm Password: '):
+			password = generate_password_hash(password, 'sha256')
+			new_user(name, email, password, superuser=True)
+			print('Superuser created')
+			exit()
+		print('Password does not match. Try again')
