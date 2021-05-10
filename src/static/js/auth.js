6f45f32deb24bbbd6690624cb810aae60e6b8c92
Prasad Biswas <alterego14@pop-os.localdomain>
2021-05-10 20:50:26 +0100

    auth html

--- src/static/js/auth.js
+++ src/static/js/auth.js
@@ -1,14 +1,22 @@
+const tabs = documents.getElementByClassName('tabby')
+
 function showNext(){
 	document.getElementsByClassName("page-1")[0].style.display="None";
 	document.getElementsByClassName("page-2")[0].style.display="block";
 }
 function tabchange(Id){
 	if(Id == "signup"){
-		document.getElementsByClassName("signup")[0].style.display="block";
+        document.getElementsByClassName("signup")[0].style.display="block";
 		document.getElementsByClassName("login")[0].style.display="None";
+
+		tabs[1].classList = ['is-active' , 'tabby'];
+        tabs[0].classList = ['tabby']
 	}
 	if(Id == "login"){
-		document.getElementsByClassName("login")[0].style.display="block";
+        document.getElementsByClassName("login")[0].style.display="block";
 		document.getElementsByClassName("signup")[0].style.display="None";
+
+		tabs[0].classList = ['is-active' , 'tabby'];
+        tabs[1].classList = ['tabby'];
 	}
 }
