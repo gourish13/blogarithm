d99cfcb7f1afa339c35819592f280573b417aad6
gourish13 <sadhu5gourish@outlook.com>
2021-05-10 23:59:05 +0530

    removed spaces

--- src/static/js/auth.js
+++ src/static/js/auth.js
@@ -1,31 +1,14 @@
 function next(){
-	
 	document.getElementsByClassName("page-1")[0].style.display="None";
 	document.getElementsByClassName("page-2")[0].style.display="block";
-	
-	
-	
 }
-
-
-
 function tabchange(Id){
-	
-	if(Id == "signup")
-	{
-		
+	if(Id == "signup"){
 		document.getElementsByClassName("signup")[0].style.display="block";
 		document.getElementsByClassName("login")[0].style.display="None";
 	}
-	
-	if(Id == "login")
-	{
-		
+	if(Id == "login"){
 		document.getElementsByClassName("login")[0].style.display="block";
 		document.getElementsByClassName("signup")[0].style.display="None";
-		
-		
 	}
-	
-	
 }
