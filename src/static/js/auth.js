d104006a4a97d0dc8fd3a6deeabb174a69378713
gourish13 <sadhu5gourish@outlook.com>
2021-05-11 02:31:42 +0530

    ✔️ tabs problem resolved

--- src/static/js/auth.js
+++ src/static/js/auth.js
@@ -1,22 +1,24 @@
-const tabs = documents.getElementByClassName('tabby')
+var tabs = document.getElementsByClassName('tabby');
 
 function showNext(){
 	document.getElementsByClassName("page-1")[0].style.display="None";
 	document.getElementsByClassName("page-2")[0].style.display="block";
 }
 function tabchange(Id){
-	if(Id == "signup"){
+	console.log(tabs)
+	if(Id === "signup"){
         document.getElementsByClassName("signup")[0].style.display="block";
 		document.getElementsByClassName("login")[0].style.display="None";
 
-		tabs[1].classList = ['is-active' , 'tabby'];
-        tabs[0].classList = ['tabby']
+		tabs[1].classList.add('is-active');
+        tabs[0].classList.remove('is-active');
 	}
-	if(Id == "login"){
+	if(Id === "login"){
         document.getElementsByClassName("login")[0].style.display="block";
 		document.getElementsByClassName("signup")[0].style.display="None";
 
-		tabs[0].classList = ['is-active' , 'tabby'];
-        tabs[1].classList = ['tabby'];
+		tabs[0].classList.add('is-active');
+        tabs[1].classList.remove('is-active');
 	}
+	console.log(tabs)
 }
