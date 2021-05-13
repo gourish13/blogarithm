6c98f75cd670b6e8173aa252da9294b5adc024b6
Rupam <www.rupamdasgupta@gmail.com>
2021-05-13 22:43:39 +0530

    create blog

--- src/blog_models.py
+++ src/blog_models.py
@@ -5,9 +5,9 @@ Model for blog related data
 from .models import db
 from datetime import datetime
 
-def new_blog(title , content , user , slug , blog_date = datetime.now()):
-    _id = db.blogs.insert(title = title ,  content = content , posted_on = blog_date , author = user , slug = slug)
-    db.commit() 
+def new_blog(title , content , user , slug , blog_date = datetime.now(),private = False):
+    _id = db.blogs.insert(title = title ,  content = content , posted_on = blog_date , author = user , slug = slug , private = private)
+    db.commit()  
+    return _id
     
-    return _id 
     
