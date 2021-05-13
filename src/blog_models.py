000b9d61cde9a686d4f0407c5805badb97cd4778
Rupam <www.rupamdasgupta@gmail.com>
2021-05-13 14:17:02 +0530

    blog creation

--- src/blog_models.py
+++ src/blog_models.py
@@ -3,7 +3,11 @@ Model for blog related data
 """
 
 from .models import db
+from datetime import datetime
 
-
-def new_blog():
-	pass
+def new_blog(title , content , user , slug , blog_date = datetime.now()):
+    _id = db.blogs.insert(title = title ,  content = content , posted_on = blog_date , author = user , slug = slug)
+    db.commit() 
+    
+    return _id 
+    
--- src/views.py
+++ src/views.py
@@ -11,7 +11,7 @@ from flask import (
     jsonify,
 )
 from slugify import slugify
-
+from .blog_models import new_blog
 
 # @desc     Home Page
 # @route    GET /
@@ -19,18 +19,22 @@ def index():
     return render_template('index.html')
 
 
-def auth():
-    return render_template('auth.html')
 
 # @desc     Blog Create
 # @route    GET /blog-create
 # @route    POST /blog-create
 def blog_create():
-    if request.method == 'GET' :
-        return render_template('blog-edit.html')
-    blog_info = dict(request.form)
-    blog_info['slug'] = slugify(request.form['blog-title'])
-    return jsonify(blog_info)
+    if 'username' in session:
+        if request.method == 'GET' :
+            return render_template('blog-edit.html')
+        blog_info = dict(request.form)
+        blog_info['slug'] = slugify(request.form['blog-title'])
+        blog_info['user'] = session['username']
+        _id = new_blog(blog_info['blog-title'] , blog_info['content'] , blog_info['user'] , blog_info['slug'])
+        view_url  = '/blog/' + blog_info['slug'] + str(_id)
+        return redirect(view_url)
+    
+    return redirect('/auth?next=blog-create')
 
 
 
@@ -39,16 +43,22 @@ def blog_create():
 # @route    GET /blog-update/<string:slug>
 # @route    POST /blog-update/<string:slug>
 def blog_update(slug):
-    if request.method == 'GET':
-        return render_template('blog-edit.html' ,title='title' , content='blogcontent')
-    blog_info = request.form
-    return jsonify(blog_info)
+    if 'username' in session:
+        if request.method == 'GET':
+            return render_template('blog-edit.html' ,title='title' , content='blogcontent')
+        blog_info = request.form
+        return jsonify(blog_info)
+    url = '/auth?next=/blog-update/?next=blog-update/'+slug 
+    return redirect(url)
 
 
 # @desc     Blog View Page
 # @route    GET /blog/<string:slug>
 def blog_view(slug):
-    return render_template('blogview.html', title='title', content='blogcontent')
+    if 'username' in session:
+        return render_template('blogview.html', title='title', content='blogcontent')
+    url = '/auth?next=blog-view/'+slug 
+    return redirect(url)
 
 
 
