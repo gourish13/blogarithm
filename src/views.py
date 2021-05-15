982d24446262e775fdb621e412dd22a9446427d3
gourish13 <sadhu5gourish@outlook.com>
2021-05-15 16:20:24 +0530

    added 404 handler

--- src/views.py
+++ src/views.py
@@ -24,19 +24,19 @@ def index():
 # @route    GET /blog-create
 # @route    POST /blog-create
 def blog_create():
+    if 'username' not in session:
+        return redirect('/auth?next=/blog-create')
     if request.method == 'GET' :
         return render_template('blog-edit.html')
-    
     blog_info = dict(request.form)
     blog_info['slug'] = slugify(request.form['blog-title'])
     if blog_info['private'] == 'true' :
         uid = new_blog(blog_info['blog-title'] , blog_info['content'] , blog_info['username'] , blog_info['slug'] ,private = True)
     else:
         uid = new_blog(blog_info['blog-title'] , blog_info['content'] , blog_info['username'] , blog_info['slug'])
-    
-    
-    view_url  = '/blog/' + blog_info['slug'] + '-' + str(uid)
-    return redirect(view_url)
+    blog_info['slug'] += '-' + str(uid)
+    # view_url  = '/blog/' + blog_info['slug'] + '-' + str(uid)
+    return redirect(f'''/blog/{blog_info['slug']}''')
     
 
 
@@ -48,8 +48,8 @@ def blog_update(slug):
         if request.method == 'GET':
             return render_template('blog-edit.html' ,title='title' , content='blogcontent')
         return jsonify(request.form)
-    url = '/auth?next=blog-update/'+slug 
-    return redirect(url)
+    # url = '/auth?next=blog-update/'+slug 
+    return redirect(f'/auth?next=blog-update/{slug}')
 
 
 # @desc     Blog View Page
@@ -63,3 +63,8 @@ def blog_view(slug):
 # @route    GET /privacy-policy
 def privacy_policy():
     return render_template('privacy-policy.html')
+
+
+# @desc     404 Page Not Found
+def page_not_found_404(error):
+    return render_template('404.html')
