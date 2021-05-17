0d534101af2858fb11a486d93f0f9124bbeaae13
gourish13 <sadhu5gourish@outlook.com>
2021-05-18 02:44:51 +0530

    added scroll arrow animation

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -2,10 +2,14 @@
   window.onload = function() {
     const nav = document.getElementsByTagName('nav')[0];
     const header = document.getElementsByClassName('header')[0];
+    const arrow = document.getElementsByClassName('arrow-container')[0];
     window.onscroll = function() {
       let scrollVal = window.pageYOffset;
-      if (scrollVal === 0)
+      arrow.classList.add('hide-arrow-container');
+      if (scrollVal === 0) {
+        arrow.classList.remove('hide-arrow-container');
         nav.classList = ['nav-hidden'];
+      }
       else if (scrollVal <= header.clientHeight && scrollVal > 0 && !nav.classList.contains('nav-transparent'))
         nav.classList = ['nav-transparent'];
       else if (scrollVal > header.clientHeight)
